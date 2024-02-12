46aa3c3b03cc3c3ca15e2c9be759c84a21fa95f3
Mathias Mogensen <mathias@appflowy.io>
2024-02-12 17:01:50 +0100

    chore: fix environment var setup

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied()
+@Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'ACCUWEATHER_API_KEY', defaultValue: 'EMPTY')
   static const String accuweatherApiKey = _Env.accuweatherApiKey;
